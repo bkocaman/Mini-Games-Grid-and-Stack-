8d73cf8b5b27e32a2c483d5d3d33d697ad935e35
bkocaman <berkkocaman@hotmail.com>
2022-11-08 13:37:20 +0300

    Some Bugs Fixed and UIManager Added

---
+++ Assets/Proje1/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public TMP_Text scoreText;
+    public TMP_InputField sizeInput;
+
+    private int score = 0;
+
+    public delegate void OnRebuildButtonClick(int count);
+    public static event OnRebuildButtonClick onRebuildButtonClick;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore(int _addition)
+    {
+        score += _addition;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void onClickRebuildButton()
+    {
+        if (int.TryParse(sizeInput.text, out int size))
+        {
+            if (size > 1)
+            {
+                if (size > 50)
+                    onRebuildButtonClick(50);
+                else
+                    onRebuildButtonClick(size);
+
+                score = 0;
+                scoreText.text = "Score: " + score;
+
+            }
+        }
+        sizeInput.text = "";
+
+    }
+}
+
